63ac6bbbb6c68f4d70959dc5387aad6be391b900
Shaily Priyadarshini <priyadarshinishaily@gmail.com>
2024-01-30 00:45:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+const products = [
+  {
+    id: 101,
+    name: "Skirt",
+    image: "https://i.pravatar.cc/48?u=118836",
+    price: 700,
+  },
+  {
+    id: 102,
+    name: "Jeans",
+    image: "https://i.pravatar.cc/48?u=933372",
+    price: 1500,
+  },
+  {
+    id: 103,
+    name: "Top",
+    image: "https://i.pravatar.cc/48?u=499476",
+    price: 1000,
+  },
+];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RetriveProducts products={products} />
+    </div>
+  );
+}
+function RetriveProducts({ products }) {
+  return (
+    <div className="container">
+      {products.map((product) => (
+        <ProductList
+          key={product.id}
+          image={product.image}
+          name={product.name}
+          price={product.price}
+        />
+        // console.log(product.name);
+      ))}
     </div>
   );
 }
 
+function ProductList({ image, name, price }) {
+  return (
+    <div className="app">
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+      <span>{price}</span>
+      <button>Add to cart</button>
+    </div>
+  );
+}
 export default App;
